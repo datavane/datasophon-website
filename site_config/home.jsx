714cab17c2d4cfe67d80fa382809c32aca09bb5a
dygao2 <dygao2@iflytek.com>
2022-10-19 18:29:45 +0800

    first

---
+++ site_config/home.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+
+export default {
+  'zh-cn': {
+    brand: {
+      brandName: 'DataSophon',
+      briefIntroduction: '快速实现大数据基础组件的自动化安装、管理、监控和告警，帮助您快速构建起大数据的分析处理能力。',
+      buttons: [
+        {
+          text: '立即开始',
+          link: '/zh-cn/docs/demo1.html',
+          type: 'primary',
+        },
+        {
+          text: '查看Github',
+          link: 'https://github.com/dygao2/datasophon',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: '开源产品的介绍',
+      desc: '开源产品的简单介绍，提供一些该产品的优点、特性描述等',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: '特性一览',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: '国产化适配',
+          content: '国产化适配，支持arm和x86机器混合部署，支持常用国产化操作系统。',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: '全面兼容开源生态',
+          content: '全面兼容开源生态，基于开放式的管理架构，DataSophon已集成13种常用大数据组件，包括数据集成、数据存储、计算引擎、任务调度、权限管理等大数据处理的各个环节。',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: '简单易用',
+          content: '极易部署，用户仅需几步即可完成初始化环境配置，1小时内即可完成整体大数据集群部署。',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: '指标丰富',
+          content: '监控指标全面丰富，基于生产实践展示用户最关心的监控指标，支持集群整体监控和组件级别监控。',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: '告警便捷',
+          content: '灵活便捷的告警服务，可实现用户自定义告警组和告警指标。',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: '特性6',
+          content: '特性6的简单概括',
+        },
+      ],
+    },
+    start: {
+      title: '快速开始',
+      desc: '简单描述',
+      img: '/img/quick_start.png',
+      button: {
+        text: '阅读更多',
+        link: '/zh-cn/docs/demo1.html',
+      },
+    },
+    users: {
+      title: '用户',
+      desc: <span>简单描述</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+  'en-us': {
+    brand: {
+      brandName: 'DataSophon',
+      briefIntroduction: 'Quickly realize the automatic installation, management, monitoring and alarm of the basic components of big data, and help you quickly build the analysis and processing capacity of big data.',
+      buttons: [
+        {
+          text: 'Quick Start',
+          link: '/en-us/docs/demo1.html',
+          type: 'primary',
+        },
+        {
+          text: 'View on Github',
+          link: 'https://github.com/dygao2/datasophon',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: 'introduction title',
+      desc: 'some introduction of your product',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: 'Feature List',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: 'feature1',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: 'feature2',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: 'feature3',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: 'feature4',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: 'feature5',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: 'feature6',
+          content: 'feature description',
+        }
+      ]
+    },
+    start: {
+      title: 'Quick start',
+      desc: 'some description text',
+      img: '/img/quick_start.png',
+      button: {
+        text: 'READ MORE',
+        link: '/en-us/docs/demo1.html',
+      },
+    },
+    users: {
+      title: 'users',
+      desc: <span>some description</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+};
