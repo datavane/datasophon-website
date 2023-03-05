ad38c51f662940533d2feacb756cddc1730f31a6
gaodayu168 <gaodayu2022@163.com>
2023-03-05 21:29:02 +0800

    test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -31,6 +31,33 @@ const FeatureList = [
       </>
     ),
   },
+  {
+    title: <Translate>便捷运维</Translate>,
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        DataSophon可方便地对集群、节点和服务进行监控和运维操作，助您大幅提升运维工作效率，让数据工程师更专注于业务开发。
+      </>
+    ),
+  },
+  {
+    title: <Translate>安全可靠</Translate>,
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        DataSophon支持Kerberos身份认证和数据加密，使用Ranger数据访问控制。支持数据加密，保证数据安全。
+      </>
+    ),
+  },
+  {
+    title: <Translate>便捷告警</Translate>,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        支持告警组和告警指标管理，监控指标全面丰富，用户可根据监控指标自定义告警组和告警指标。
+      </>
+    ),
+  },
 ];
 
 function Feature({Svg, title, description}) {
